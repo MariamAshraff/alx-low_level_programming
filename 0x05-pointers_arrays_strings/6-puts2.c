dbb9156b76d140c89ff3ccb7bd064d604a9b33ac
Mariam_Ashraf <mariamashrafff163@.com>
2023-03-27 19:22:02 -0700

    m

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints one char out of 2 of a string
+ * @str: string imp.
+ */
+void puts2(char *str)
+{
+	int j, i = 0;
+
+
+	while (trs[i])
+	{
+		i++;
+	}
+
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
+	}
+
+	_putchar('\n');
+}
