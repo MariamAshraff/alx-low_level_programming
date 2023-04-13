d765c0eddd67e6f8676b62a1bf305a38a9451408
Mariam_Ashraf <mariamashrafff163@.com>
2023-04-13 08:02:29 -0700

    Added task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,39 +5,39 @@
  * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
- * @n: number of bytes of s2
+ * @n: number of bytes of s2 to concatenate
  *
- * Return: pointer to the concatenated string or NUll
+ * Return: pointer to the concatenated string or 0
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	if (s1 == NULL)
-	{
-	s1 = "";
-	}
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s2 == NULL)
-	{
-	s2 = "";
-	}
-	size_t len1 = strlen(s1);
-	size_t len2 = strlen(s2);
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
 
 	if (n >= len2)
-	{
-	n = len2;
-	}
-	char *result = malloc((len1 + n + 1) * sizeof(char));
+		n = len2;
 
-	if (result == NULL)
-	{
+	s = malloc(sizeof(char) * (len1 + n + 1));
+
+	if (s == NULL)
 		return (NULL);
+
+	for (i = 0; i < len1; i++)
+	{
+	s[i] = s1[i];
 	}
 
-	memcpy(result, s1, len1);
-	memcpy(result + len1, s2, n);
+	for (j = 0; j < n; j++)
+	{
+	s[i + j] = s2[j];
+	}
 
-	result[len1 + n] = '\0';
+	s[i] = '\0';
 
-	return (result);
+	return (s);
 }
